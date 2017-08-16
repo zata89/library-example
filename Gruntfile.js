6c971b89b54c6234e4b6946538a22cc5cb9b9a73
zata89 <zata1989@gmail.com>
2017-08-16 09:24:58 -0500

    Update of grunt and finish the page

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,17 +23,77 @@ module.exports = function(grunt) {
         }]
       }
     },
+    htmlmin: {  
+      dev: {
+          files: [{
+            expand: true,
+            cwd: 'dev/html',
+            src: ['**/*.html'],
+            dest: 'public'
+        }]
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'dev/img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'public/img/'
+        }]
+      }
+    },
+    uglify: {
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: 'dev/js',
+          src: '**/*.js',
+          dest: 'public/js'
+        }]
+      }
+    },
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: 'http://localhost:9001',
+              replacement: ''
+            },
+            {
+              match: '.css',
+              replacement: '.min.css'
+            }
+          ]
+        },
+        files: [
+          {
+            expand: true, 
+            flatten: true, 
+            src: ['dev/html/**/*.html'], 
+            dest: 'dev/processedHtml'
+          }
+        ]
+      }
+    },
     connect: {
-      server: {
+      testPageServer: {
         options: {
           port: 9000,
           base: 'dev/html'
         }
       },
-      server2: {
+      testMediaServer: {
         options: {
           port: 9001,
           base: 'dev'
+        }
+      },
+      publicPageServer: {
+        options: {
+          port: 9004,
+          base: 'public'
         }
       }
     },
@@ -50,10 +110,17 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-replace');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('default', ['connect:testPageServer', 'connect:testMediaServer', 'watch']);
   grunt.registerTask('createStyles', ['sass', 'cssmin']);
-
+  grunt.registerTask('runPublicServer', function(){
+    grunt.task.run('connect:publicPageServer:keepalive');
+  });
+  grunt.registerTask('createPublic', ['sass', 'cssmin', 'htmlmin', 'imagemin', 'uglify']);
 };
